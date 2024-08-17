ab2bbeb697cb5259f415c9f5c478c24a8d4bf7fa
Aniket-Singh-Baghelgit config --global user.email aniketsinghbaghel211@gamil.comgit config --global user.name Aniket-Singh-Baghel <aniketsinghbaghel211@gamil.com>
2024-08-18 01:42:24 +0530

    flavour section complete

--- src/Components/Flavours.jsx
+++ src/Components/Flavours.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaLongArrowAltUp } from "react-icons/fa";
+import { IoIosArrowRoundUp } from "react-icons/io";
 import whitePeach from "../assets/whitePeach.png";
+// import whitePeach from "../assets/handeWhitePeach.png";
 import greenMint from "../assets/grenMint.png";
 import Blackberry from "../assets/blackCoke.png";
 import blueRaspberry from "../assets//BlueRaspberry.png";
@@ -38,7 +40,7 @@ function Flavours() {
         {questions.map((i, index) => {
           return (
             <div className="bg-white shadow-lg border border-[#E0F7FA] w-full rounded-3xl px-6 py-4 flex flex-col items-start">
-              <button className="bg-[#C6FF00] text-[#333333] px-4 py-2 rounded-md mb-4">
+              <button className="bg-[#C6FF00] text-[#333333] font-semibold px-4 py-2 rounded-md mb-4">
                 {i.button}
               </button>
               <div className="w-full flex mt-6">
@@ -59,15 +61,18 @@ function Flavours() {
             Ultimate Fizzo <br /> Fantasy
           </h1>
           <div className="image bg-[#F6A226] h-[80vh] ml-[5vw] relative rounded-3xl">
-            <img
-              src={whitePeach}
-              alt="GoToFlavour"
-              className="h-[92vh] absolute -top-20"
-            />
-            <div className="p-4 bg-[#70707033] absolute bottom-0 rounded-bl-3xl rounded-br-3xl">
-              <h2 className="peachText text-[10vh] font-bold text-white leading-none capitalize">
-                Pearl White Peach
-              </h2>
+            <div className="top-4 left-10 w-16 h-16 rotate-90 border-2 rounded-full relative flex items-center justify-center">
+              <IoIosArrowRoundUp className="text-white text-4xl"/>
+            </div><a href="#">
+            <div className="h-[90vh] absolute bottom-0">
+              <img src={whitePeach} alt="GoToFlavour" className="h-full w-full object-cover" />
+            </div></a>
+            <div className="absolute w-full bottom-0 bg-[#f6a32677] backdrop-blur-sm rounded-bl-3xl  rounded-br-3xl overflow-hidden">
+              <div className="py-4 px-16 flex items-center justify-center">
+                <h2 className="peachText text-[8vh] font-bold text-white leading-none capitalize">
+                  Pearl White Peach
+                </h2>
+              </div>
             </div>
           </div>
         </div>
@@ -79,7 +84,7 @@ function Flavours() {
                   className={
                     index == 1
                       ? "h-1/2 mt-8 bg-[#073B58] rounded-3xl relative"
-                      : "h-1/2 mt-8 bg-[#E52184] rounded-3xl relative"
+                      : "h-1/2 mt-8 bg-[#FF6F61] rounded-3xl relative"
                   }
                 >
                   <img
@@ -102,8 +107,8 @@ function Flavours() {
                 <div
                   className={
                     index == 1
-                      ? " h-[51vh] bg-[#009689] rounded-3xl mt-8 relative "
-                      : "h-1/2 mt-8 bg-[#28B8CE] rounded-3xl relative "
+                      ? " h-[51vh] bg-[#1d90c5] rounded-3xl mt-8 relative "
+                      : "h-1/2 mt-8  bg-[#009689] rounded-3xl relative "
                   }
                 >
                   <img
@@ -115,7 +120,7 @@ function Flavours() {
                         : "h-[48vh] w-[20vw] pt-10 absolute bottom-24"
                     }
                   />
-                  <div className="p-4 bg-[#70707033] w-full flex justify-center absolute bottom-0 rounded-bl-3xl rounded-br-3xl">
+                  <div className="p-4 bg-[#70707033]  w-full flex justify-center absolute bottom-0 rounded-bl-3xl rounded-br-3xl">
                     <h2 className="peachText text-[3vh] font-bold text-white leading-none capitalize">
                       {i.title}
                     </h2>
@@ -129,7 +134,7 @@ function Flavours() {
       <div className="flex justify-center items-center h-[20vh]">
         <button className="w-[20vw] mt-4 h-[10vh] bg-[#C6FF00] flex  items-center px-5 rounded-lg text-[1.5rem] text-[#181b20] font-semibold">
           Taste the Magic
-            <FaLongArrowAltUp className="rotate-90 h-16 font-light ml-7 mt-2" />
+          <FaLongArrowAltUp className="rotate-90 h-16 font-light ml-7 mt-2" />
         </button>
       </div>
     </div>
@@ -137,4 +142,3 @@ function Flavours() {
 }
 
 export default Flavours;
-
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,13 @@ import logo2 from "../assets/logo2.png"
 
 function Navbar() {
   return (
-    <div className="w-full bg-[#f8feff] flex justify-between py-3 px-16 text-[#333333] outline-none fixed z-[999]">
+    <div className="w-full bg-[#f8feff] flex justify-between py-3 px-16 text-[#333333] fixed z-[999]">
       <img src={logo} alt="logo" className="h-10"/>
       <div className="links flex gap-12">
         {["Flavours", "Shop", "About Us", "Blog", "contact Us"].map(
           (i, index) => {
             return (
-             <a href="#" key={index} className={`text-md capitalize ${index===4 && "ml-32"}`}>{i}</a>
+             <a href="#" key={index} className={`text-md capitalize font-medium outline-none ${index===4 && "ml-32"}`}>{i}</a>
             );
           }
         )}
